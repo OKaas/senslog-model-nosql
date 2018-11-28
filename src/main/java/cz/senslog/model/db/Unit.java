f3d108d2b12bd894d205ff2f2303fb22f3b976ea
LudekKanak <ludek.kanak@seznam.cz>
2018-11-28 21:14:14 +0100

    Add uid to Unit

--- src/main/java/cz/senslog/model/db/Unit.java
+++ src/main/java/cz/senslog/model/db/Unit.java
@@ -2,15 +2,21 @@ package cz.senslog.model.db;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
-@NoArgsConstructor
+
 @Document(collection = "unit")
 public class Unit {
-
-    @Id
-    private String uid;
+	@Id
+    private ObjectId _id;
 	private String description;
+	
+	public Unit() {
+        this._id = ObjectId.get();
+    }
+	
 }
