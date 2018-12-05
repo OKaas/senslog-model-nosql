c70fbac7a4f4cc8caf1233313af0e061191098a2
Tomáš Šimandl <tomas.simandl@gmail.com>
2018-12-05 18:01:34 +0100

    added equals method to Unit

--- src/main/java/cz/senslog/model/db/Unit.java
+++ src/main/java/cz/senslog/model/db/Unit.java
@@ -5,6 +5,8 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.Objects;
+
 @Data
 @Document(collection = "unit")
 public class Unit {
@@ -16,4 +18,16 @@ public class Unit {
         this.uid = ObjectId.get();
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Unit unit = (Unit) o;
+        return Objects.equals(uid, unit.uid);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(uid);
+    }
 }
